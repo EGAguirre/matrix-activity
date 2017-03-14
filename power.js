b34b8dc4ac2ff4419f7552a4f3f595bf400182c0
EGAguirre <9eaguirre@stpatrick.org>
2017-03-14 10:48:18 -0500

    worked with kristoff
    Finished

--- power.js
+++ power.js
@@ -1,6 +1,5 @@
 const Matrix = require("matrix-js");
 
-
 let A = Matrix([
     [0,1,1,1],
     [1,0,1,0],
@@ -11,12 +10,12 @@ let A = Matrix([
 function power(mtrx,p){
     //mtrx is the matrix you input
     //p is the power of the matrix you want
-    while p < 10
-    let squared = matrix(A.prod(A));//A^2
-    let fourth = matrix(squared.prod(squared));//A^4
-    let eight = matrix(fourth.prod(fourth));//A^8
-    let tenth = matrix(fifth.prod(fifth));//A^10
-
+    let mtrx = A
+    let p = p
+    for(a=0;a<p;p++){
+      let mtrx(mtrx.prod(mtrx));
+    }
+  }
 }
 
 
